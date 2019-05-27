4ff22f49cf234748d4501e575e5d7a188dad5253
Ankit Taneja <ankittaneja@Ankits-MacBook-Pro.local>
2019-05-27 17:49:07 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const render = require('koa-ejs'); 
+const path = require('path');
+const Router = require('koa-router');
+const jsForce = require('jsforce');
+
+const app = module.exports = new Koa();
+
+var router = new Router();
+
+app.use(koaBody());
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt: 'html',
+  cache: false,
+  debug: false
+});
+
+router.get('/', async (ctx, next) => {
+   ctx.body = 'Hello World!';
+});
+
+var username = '';
+var password = '';
+
+var jsforce = require('jsforce');
+var conn = new jsforce.Connection({
+  // you can change loginUrl to connect to sandbox or prerelease env.
+  // loginUrl : 'https://test.salesforce.com'
+});
+// conn.login(username, password, function(err, userInfo) {
+//   if (err) { return console.error(err); }
+//   // Now you can get the access token and instance URL information.
+//   // Save them to establish connection next time.
+//   console.log(conn.accessToken);
+//   console.log(conn.instanceUrl);
+//   // logged in user property
+//   console.log("User ID: " + userInfo.id);
+//   console.log("Org ID: " + userInfo.organizationId);
+//   // ...
+// });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
